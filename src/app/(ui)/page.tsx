72e66a2d874d84d06e8f0cd2932f2ce6e67255be
avashForReal <avash700@gmail.com>
2025-06-25 11:24:21 +0545

    feat: landing page

--- src/app/(ui)/page.tsx
+++ src/app/(ui)/page.tsx
@@ -3,18 +3,31 @@
 import { Button } from "@/components/ui/button";
 import UpgradePlan from "@/components/modal/upgrade-plan";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
-  const [showUpgradeModal, setShowUpgradeModal] = useState(true);
+  const [showUpgradeModal, setShowUpgradeModal] = useState(false);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Button onClick={() => setShowUpgradeModal(true)}>Upgrade plan</Button>
+    <>
+      <div className="w-[100vw] h-[100vh] bg-primary-100">
+        <div className="flex flex-row items-center justify-between px-[100px] py-[20px]">
+          <div className="flex flex-row items-center justify-center gap-3">
+            <Image src="/logo.svg" alt="logo" width={28} height={28} />
+            <h1 className="text-white text-lg font-medium">MusicGPT</h1>
+            <h1 className="text-white text-sm font-medium border-1 rounded-md px-3">Beta</h1>
+          </div>
+          <div>
+            <Button
+              onClick={() => setShowUpgradeModal(true)}
+              className="cursor-pointer w-full rounded-md gap-2 px-3 py-[3px] bg-white hover:bg-white border border-primary-500 text-primary-100 font-semibold text-[14px]"
+            >
+              Upgrade
+            </Button>
+          </div>
         </div>
-      </main>
+      </div>
       <UpgradePlan showUpgradeModal={showUpgradeModal} setShowUpgradeModal={setShowUpgradeModal} />
-    </div>
+    </>
   );
 }
